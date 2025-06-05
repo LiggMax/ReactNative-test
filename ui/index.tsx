63ef17b0f37b2c907659d7ac3569e179ab635ccd
LiggPlus <wenzhouli06@gmail.com>
2025-06-05 13:22:27 +0800

    容器布局

--- ui/index.tsx
+++ ui/index.tsx
@@ -4,12 +4,36 @@ import {StyleSheet, Text, View} from 'react-native';
 export default class index extends Component<{}, {}> {
   render() {
     return (
-      <View style={{flex: 0, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 30, color: 'red'}}>Hello World</Text>
-        <Text style={{fontSize: 30, backgroundColor: 'red'}}>Hello World</Text>
+      <View>
+        <View>
+          <Text style={{fontSize: 40, marginHorizontal: 20}}>布局</Text>
+          <Text style={{fontSize: 30, color: 'red'}}>Hello World</Text>
+          <Text style={{fontSize: 30, backgroundColor: 'red'}}>
+            Hello World
+          </Text>
+          <Text style={[styles.instructions]}>Hello React Native</Text>
+        </View>
 
-        <Text style={[styles.welcome]}>Hello React Native</Text>
-        <Text style={[styles.instructions]}>Hello React Native</Text>
+        <Text style={[styles.title]}>反转布局</Text>
+        <View style={[styles.flexColumnReverse]}>
+          <Text style={[styles.test]}>vue</Text>
+          <Text style={[styles.test]}>React</Text>
+          <Text style={[styles.test]}>React Native</Text>
+        </View>
+
+        <Text style={[styles.title]}>横向布局</Text>
+        <View style={[styles.flexRow]}>
+          <Text style={[styles.test]}>vue </Text>
+          <Text style={[styles.test]}>React </Text>
+          <Text style={[styles.test]}>React Native</Text>
+        </View>
+
+        <Text style={[styles.title]}>横向右对齐布局</Text>
+        <View style={[styles.flexRowReverse]}>
+          <Text style={[styles.test]}>vue </Text>
+          <Text style={[styles.test]}>React </Text>
+          <Text style={[styles.test]}>React Native</Text>
+        </View>
       </View>
     );
   }
@@ -23,5 +47,29 @@ const styles = StyleSheet.create({
   instructions: {
     color: '#2d68ff',
     fontWeight: 800,
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    color: '#42fcf2',
+    fontWeight: 'bold',
+  },
+  test: {
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  flexColumnReverse: {
+    borderWidth: 2,
+    borderColor: '#00ff38',
+    flexDirection: 'column-reverse',
+  },
+  flexRow: {
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: '#00ff38',
+  },
+  flexRowReverse: {
+    flexDirection: 'row-reverse',
+    borderColor: '#00ff38',
   },
 });
