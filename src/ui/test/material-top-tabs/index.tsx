93e9d3b53d23b83a3ca1e41ab4b3d4874e7b1d6f
LiggPlus <wenzhouli06@gmail.com>
2025-06-12 16:56:48 +0800

    layout 1.1

--- src/ui/test/material-top-tabs/index.tsx
+++ src/ui/test/material-top-tabs/index.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 /**
  * 推荐页面
  */
 function Recommend() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View>
-        <Text>推荐</Text>
-      </View>
-    </SafeAreaView>
+    <View>
+      <Text>推荐</Text>
+    </View>
   );
 }
 
@@ -21,11 +20,9 @@ function Recommend() {
  */
 function Hot() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View>
-        <Text>热门</Text>
-      </View>
-    </SafeAreaView>
+    <View>
+      <Text>热门</Text>
+    </View>
   );
 }
 
@@ -34,11 +31,9 @@ function Hot() {
  */
 function Rank() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View>
-        <Text>排行</Text>
-      </View>
-    </SafeAreaView>
+    <View>
+      <Text>排行</Text>
+    </View>
   );
 }
 
@@ -46,8 +41,17 @@ const MTab = createMaterialTopTabNavigator();
 export default function index() {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <MTab.Navigator>
-        <MTab.Screen name="推荐" component={Recommend} />
+      <MTab.Navigator
+      >
+        <MTab.Screen
+          name="推荐"
+          options={{
+            tabBarIcon: ({}) => (
+              <Icon name="home" size={20} color="#000" />
+            ),
+          }}
+          component={Recommend}
+        />
         <MTab.Screen name="热门" component={Hot} />
         <MTab.Screen name="排行" component={Rank} />
       </MTab.Navigator>
